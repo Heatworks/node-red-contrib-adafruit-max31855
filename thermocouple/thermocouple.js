a7e919b75efce65674b2ac438a19667f5d740966
Weston Catron <westonsoccer2000@gmail.com>
2016-12-14 09:51:54 -0500

    Attempt to read using python script.

--- thermocouple/thermocouple.js
+++ thermocouple/thermocouple.js
@@ -1,10 +1,29 @@
+var pyshell = require('python-shell');
+
 module.exports = function(RED) {
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+
+        
         this.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
             node.send(msg);
+        });
+        
+        pyshell.run('spi_read.py', function (err) {
+            if (err) throw err;
+            console.log('finished');
+        });
+        pyshell.on('message', function (message) {
+            // received a message sent from the Python script (a simple "print" statement)
+            console.log(message);
+        });
+
+        // end the input stream and allow the process to exit
+        pyshell.end(function (err) {
+            if (err) throw err;
+            console.log('finished');
         });
     }
     RED.nodes.registerType("adafruit-max31855",LowerCaseNode);
